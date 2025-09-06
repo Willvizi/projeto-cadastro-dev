824f6b4dbe1569d4322fcddc702c41f87cd92472
Willian <williavizi@gmail.com>
2025-09-05 22:14:37 -0300

    Adicionando o tsconfig do json e configurando o banco para usar migration

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -1,14 +1,14 @@
-const { DataSource } = require('typeorm');
+import { DataSource } from 'typeorm';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || '7KTu)%7',
   database: process.env.DB_NAME || 'postgres',
-  entities: [], 
+  entities: [],
   synchronize: true,
 });
 
-module.exports = AppDataSource;
+export default AppDataSource;
